e3cb76f1125f55a6724cbe4463866bc8755be173
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-22 12:07:36 +0300

    Add Joke component to App for displaying jokes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Joke from './pages/joke';
 
 function App() {
   return (
     <div className="App">
-      
+        <Joke />
     </div>
   );
 }
