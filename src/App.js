b16933531981ce66ced2a8162dbeab5d7debee5e
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-22 12:35:33 +0300

    Add heading to App.js for Joke Generator title

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Joke from './pages/joke';
 function App() {
   return (
     <div className="App">
+      <h3 className="text1">Joke Generator with React and Joke Api </h3>
         <Joke />
     </div>
   );
