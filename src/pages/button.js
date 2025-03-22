fe561eb467c1274165b658b81b3afd2b87848009
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-22 12:22:02 +0300

    Add export statement and CSS import to Button component

--- src/pages/button.js
+++ src/pages/button.js
@@ -1,5 +1,11 @@
+import React from "react";
+import './button.css'
+
+
 const Button = (props) => {
     return <button onClick={props.callApi}>
         Click to generate a joke.
     </button>;
 }
+
+export default Button;
