293d2be95285de2a08a0d1778bc7778e35298f5b
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-22 12:08:02 +0300

    Refactor Joke component to fix state variable naming and improve code structure

--- src/pages/joke.js
+++ src/pages/joke.js
@@ -1,19 +1,23 @@
-const Joke = () =>{
-    const [joke, setJoke] = React.useState("");
+import React from "react";
+ 
+import Button from "./button";
+import './joke.css';
 
-    const fetchApi = ()  => {  
-        fetch ("https://sv443.net/jokeapi/v2/joke/Programming?type=single")
-        .then((res) => res.json())
-        .then((data) => setJoke(data.joke));
-    }
+const Joke = () => {
+    const [Joke, setJoke] = React.useState("");
 
+    const fetchApi = () => {
+        fetch("https://sv443.net/jokeapi/v2/joke/Programming?type=single")
+            .then((res) => res.json())
+            .then((data) => setJoke(data.joke));
+    };
 
-        return(
-            <div className="joke">
-                <Button callApi={fetchApi} />
-                <p>{joke}</p>
-            </div>
-        )
+    return (
+        <div className="joke">
+            <Button callApi={fetchApi} /> 
+            <p>{Joke}</p>    
+        </div>
+    );
 }
 
 export default Joke;
